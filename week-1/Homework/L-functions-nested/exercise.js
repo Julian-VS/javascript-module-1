7f3372f620ab258ec5f591ffe305e223262e00d3
Julian-VS <julian.varelasilva@outlook.com>
2021-06-29 23:45:13 +0200

    L-Functions OK

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -1,5 +1,23 @@
+function percentage (n1,n2){
+return Math.round(100*n1/(n1+n2));
+}
+
+function StringGen(pcnt){
+    return "El porcentaje de alumnos es de " + pcnt + " % mientras que el de mentores es de " + (100-pcnt) + " %.";
+}
+
 var mentor1 = "Daniel";
 var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+var numStu = 50;
+var numMent = 5;
+
+pcntStu = percentage(numStu, numMent);
+
+message = StringGen(pcntStu)
+
+console.log(message)
+
