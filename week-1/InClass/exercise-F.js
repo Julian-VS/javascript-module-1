d2c20068e554e683783ae037c3d9dd7ec43907cb
Julian-VS <julian.varelasilva@outlook.com>
2021-06-29 20:35:21 +0200

    Ex F - OK

---
+++ week-1/InClass/exercise-F.js
@@ -0,0 +1,4 @@
+var numStu = 15;
+var numMent = 8 ;
+
+console.log( "Number of students: " + numStu + "\nNumber of mentors: " + numMent +"\nTotal number of students and mentors: " + (numMent+numStu));
