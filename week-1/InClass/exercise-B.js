21532189c1b5421b97ed0df28e66b5e1d21f9d35
Julian-VS <julian.varelasilva@outlook.com>
2021-06-29 19:02:40 +0200

    ejercicio 2 - 5 idiomas

--- week-1/InClass/exercise-B.js
+++ week-1/InClass/exercise-B.js
@@ -1,3 +1,5 @@
 console.log("Halo, dunia! // Indonesian");
 console.log("Ciao, mondo! // Italian");
 console.log("Hola, mundo! // Spanish");
+console.log ("Hello, world! //English")
+console.log("Hallo, Welt! //German")
