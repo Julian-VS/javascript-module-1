f2836e784d7d0908b6f82b5fe6e3a20dd499756e
Julian-VS <julian.varelasilva@outlook.com>
2021-06-29 22:49:56 +0200

    F-Strings Ex 1 - OK

--- week-1/Homework/F-strings-methods/exercise.js
+++ week-1/Homework/F-strings-methods/exercise.js
@@ -1,3 +1,7 @@
-// Start by creating a variable `message`
+// A la longitud del string de mi nombre, le resto 3 debido a los espacios " "
+
+const nam = "Julian Ramón Varela Silva";
+
+var message = "My name is "+ nam +" and my name is " + (nam.length-3) + " characters long";
 
 console.log(message);
