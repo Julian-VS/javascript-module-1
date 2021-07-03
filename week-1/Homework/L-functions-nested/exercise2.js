4581555ff9519f4e4ca007c215129eca18512045
Julian-VS <julian.varelasilva@outlook.com>
2021-07-03 11:42:27 +0200

    Updated ex2 - OK

---
+++ week-1/Homework/L-functions-nested/exercise2.js
@@ -0,0 +1,32 @@
+function upper_nam (nam){
+    return nam.toUpperCase(nam);
+}
+
+function hi (ment_nam){
+    return "HELLO " + ment_nam;
+}
+
+var mentor1 = "Daniel";
+var mentor2 = "Irina";
+var mentor3 = "Mimi";
+var mentor4 = "Rob";
+var mentor5 = "Yohannes";
+
+NAM1 = upper_nam(mentor1);
+NAM2 = upper_nam(mentor2);
+NAM3 = upper_nam(mentor3);
+NAM4 = upper_nam(mentor4);
+NAM5 = upper_nam(mentor5);
+
+Greet1 = hi (NAM1);
+Greet2 = hi (NAM2);
+Greet3 = hi (NAM3);
+Greet4 = hi (NAM4);
+Greet5 = hi (NAM5);
+
+console.log(Greet1);
+console.log(Greet2);
+console.log(Greet3);
+console.log(Greet4);
+console.log(Greet5);
+
