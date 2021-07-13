b677087756617cf320e54e3ddd3e93f34651032f
Julian-VS <julian.varelasilva@outlook.com>
2021-07-13 21:47:32 +0200

    ExH OK

---
+++ week-3/InClass/exercise-H.js
@@ -0,0 +1,3 @@
+const names = ["Daniel", "James", "Irina", "Mozafar", "Ashleigh", "Julian"];
+let MyName = "Julian"
+console.log(names.find(name => name==="Julian")? "Found me !" : "Didnt finde me :(")
