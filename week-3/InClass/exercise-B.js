34065d0f27483c8d89d3f41e554495ca1234faeb
Julian-VS <julian.varelasilva@outlook.com>
2021-07-13 19:06:17 +0200

    Class ExB OK

---
+++ week-3/InClass/exercise-B.js
@@ -0,0 +1,21 @@
+function print_sorted_list (arr){
+    console.log(`La lista ordenada alfabeticamente es: ${arr.sort()}`)
+    }
+
+function search (name, arr1, arr2){
+    if (arr1.includes(name)){
+        console.log(`Si, ${name} pertence a la primer clase junto con: ${arr1}`)
+    } else if (arr2.includes(name)){
+        console.log(`Si, ${name} pertence a la segunda clase junto con: ${arr2}`)
+    } else {
+        console.log(`No, ${name} no pertence a ninguna clase.`)
+    }
+}
+
+let names1 = ['Julian', 'Juan', 'Matias', 'Maria Victoria', 'Wendy', 'Jesus'];
+let names2 = ['Caleb', 'Gabriela', 'Génesis', 'Giomar', 'Jaime'];
+
+print_sorted_list(names1);
+
+search("Julian", names1, names2)
+
