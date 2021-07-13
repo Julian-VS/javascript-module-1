505c829b55808270bf1e462a983f0cff35023ad2
Julian-VS <julian.varelasilva@outlook.com>
2021-07-13 21:22:46 +0200

    Ex F OK - OneLiner

--- week-3/InClass/exercise-F.js
+++ week-3/InClass/exercise-F.js
@@ -1,9 +1,7 @@
-
 let BirthYear = [1964, 2008, 1999, 2005, 1978, 1985, 1919];
+let year = new Date().getFullYear();
 
-const AGES = BirthYear.map(age => 2021-age);
-const GreenLight = AGES.map(num => num >= 17 ?  "Born in " + (2021-num) + " can drive": "Born in " + (2021-num) + " can drive in " +(17-num)+" years.")
-
+const GreenLight = BirthYear.map(num => num <= (year-17) ?  "Born in " + (num) + " can drive.": "Born in " + (num) + " can drive in " +(num-(year-17))+" years.")
 
 console.log (GreenLight)
 
