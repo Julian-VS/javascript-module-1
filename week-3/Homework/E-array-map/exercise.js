ec152d9178591fff0f1a522f7774aa83c8a8776c
Julian-VS <julian.varelasilva@outlook.com>
2021-07-17 12:27:53 +0200

    E-array OK

--- week-3/Homework/E-array-map/exercise.js
+++ week-3/Homework/E-array-map/exercise.js
@@ -1,6 +1,11 @@
 // Using the .map() method, create a new array with `numbers` multiplied by 100
 // Write multiple solutions using different syntax (as shown in the README)
 
+function double (n){
+    return n*2
+}
+
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
 
-console.log(numbers.map(n => n*100))
+var hundNumb = numbers.map(n => n*100);
+console.log(hundNumb);
